ddacacdddbfbd77bef76059aec3b471b1b866b85
Tjoosten <Tjoosten3@gmail.com>
2016-08-23 19:18:37 +0200

    Add signatures model, factory, migration

---
+++ app/Signatures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Signatures
+ * @package App
+ */
+class Signatures extends Model
+{
+    /**
+     * Mass-assign fields.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'birth_date', 'address', 'city'];
+}
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,3 +21,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Signatures::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'birth_date' => $faker->date('Y-m-d'),
+        'address' => $faker->address,
+        'city' => $faker->city,
+    ];
+});
