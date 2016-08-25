d936c72bc2ddd2ac5678561f5be39003f98d9395
Tjoosten <Tjoosten3@gmail.com>
2016-08-26 00:54:31 +0200

    Implement log

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Statistics;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->logVisit();
+    }
+
+    public function logVisit()
+    {
+        Statistics::create([
+            'route' =>  Request::url(),
+            'ip_address' => Request::ip()
+        ]);
+    }
 }
