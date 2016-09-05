22cd10205029ac1ce46c4a885973bd1b3f159d0b
Tim Joosten <Tjoosten3@gmail.com>
2016-09-05 20:19:23 +0200

    Add specific error view

--- resources/views/feedback/show.blade.php
+++ resources/views/feedback/show.blade.php
@@ -3,24 +3,63 @@
 @section('content')
     <div class="container">
         <div class="row">
-            {{-- Tab menu --}}
-            <ul class="nav nav-tabs" role="tablist">
-                <li role="presentation" class="active">
-                    <a href="#home" aria-controls="home" role="tab" data-toggle="tab">
-                        Rapport
-                    </a>
-                </li>
+            <div class="col-sm-9">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Globale info:</div>
 
-                <li role="presentation">
-                    <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">
-                        Updates/Commentaar
-                    </a>
-                </li>
-            </ul>
-            {{-- /Tab menu --}}
+                    {{-- Ticket info --}}
+                    <div class="panel-body">
+                        <h4>Raporterings info:</h4>
 
-            {{-- Tab content --}}
-            {{-- /Tab content --}}
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td style="width: 150px;"><strong>Gecreerd door:</strong></td>
+                                <td>{{ $item->naam }}</td>
+                            </tr>
+                            <tr>
+                                <td style="width: 150px;"><strong>Persoon email:</strong></td>
+                                <td>{{ $item->email }}</td>
+                            </tr>
+                            <tr>
+                                <td style="width: 150px;"><strong>Categorie:</strong></td>
+                                <td>{{ $item->category->categorie }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+
+                        <h4 style="margin-top: 20px;">Gebruikers feedback:</h4>
+                        {{ $item->melding }}
+                    </div>
+                </div>
+                {{-- /Ticket info --}}
+            </div>
+
+            <div class="col-sm-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Ticket Info.</div>
+
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            Ticket ID: <span class="pull-right"><strong><code>#T{{ $item->id }}</code></strong></span>
+                        </li>
+                        <li class="list-group-item">
+                            Gecreerd op:
+
+                            <span class="pull-right">
+                                <strong>{{ Carbon\Carbon::parse($item->created_at)->format('d-m-Y') }}</strong>
+                            </span>
+                        </li>
+                        <li class="list-group-item">
+                            Update op:
+
+                            <span class="pull-right">
+                                <strong>{{ Carbon\Carbon::parse($item->updated_at)->format('d-m-Y') }}</strong>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
